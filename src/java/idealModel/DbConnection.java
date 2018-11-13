1a274f262253211ff10badec9931f023328e67fd
Björn Bergdagl <molnet@gmail.com>
2018-11-13 13:59:02 +0100

    Initial commit, working API and db

---
+++ src/java/idealModel/DbConnection.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idealModel;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnection {
+    public static String HOST = "jdbc:mysql://localhost:3306/ideal?autoReconnect=true&useSSL=false";
+    public static String USERNAME = "root";
+     public static String PASSWORD = "Remington870E";
+//    public static String PASSWORD = "meiofasknasmisse123";
+    private static Connection con;
+       
+    public void dbConnect() throws SQLException, ClassNotFoundException {
+        
+        if (con == null)    {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(HOST, USERNAME, PASSWORD);   
+        }
+    }
+    
+        
+    public void closeDbConnection() throws SQLException {
+        con.close();
+    }
+    
+    public void writeSp(String Sp) throws SQLException   {
+        String sql = "{call " + Sp + "}";    
+        try {
+            dbConnect();
+            CallableStatement call = con.prepareCall(sql);
+            call.executeUpdate();
+        }
+        catch (ClassNotFoundException cfe)   {
+            System.out.println("Class not found"+ cfe);
+        }
+                  
+    }
+    
+    public ResultSet readSp(String Sp)  throws SQLException {
+        ResultSet toReturn = null; 
+        String sql = "{call " + Sp + "}";
+        try {
+            dbConnect();
+            CallableStatement call = con.prepareCall(sql);
+            toReturn= call.executeQuery();
+        }
+        catch (ClassNotFoundException cfe)   {
+            System.out.println("Class not found"+ cfe);
+        }
+        return toReturn;
+    }
+    
+    public ResultSet runSql(String sql){
+        ResultSet rs = null;
+        try {
+            dbConnect();
+            Statement stmt = con.createStatement();
+            rs = stmt.executeQuery(sql);
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage( ));
+        }   
+        catch (ClassNotFoundException cfe)   {
+            System.out.println("Class not found"+ cfe);
+        }
+        return rs;
+    }
+}
---
+++ src/java/idealModel/IdealId.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idealModel;
+
+import idealWS.IdealWS;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Molnet
+ */
+public class IdealId {
+    private int id;
+    
+    private IdealId(){}
+    
+    public IdealId(String ideal, String enrollCode)    {
+        DbConnection db = new DbConnection();
+        setId(999);
+          
+        try {
+            ResultSet rs = db.runSql("SELECT Id FROM ideal.studentenrolled WHERE Ideal = '"
+                    + ideal
+                    + "' AND EnrollCode = '"
+                    + enrollCode
+                    + "';");
+            while (rs.next()) {
+                setId(rs.getInt("Id"));
+            }
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(IdealWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+}
