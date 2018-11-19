ad304cc74b49de0b985208bc217986531d17cddc
Björn Bergdagl <molnet@gmail.com>
2018-11-19 14:03:46 +0100

    Added sql-testdata

---
+++ src/res/ideal.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM ideal.studentenrolled
+LIMIT 0, 1000
+
+-- Date: 2018-11-13 13:22
+*/
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (1,'bjrber-7','LTU10001');
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (2,'bjrber-7','LTU10004');
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (3,'andkuo-7','LTU10002');
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (4,'andkuo-7','LTU10005');
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (5,'epijoa-7','LTU10012');
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (6,'epijoa-7','LTU10009');
+INSERT INTO `studentenrolled` (`Id`,`Ideal`,`EnrollCode`) VALUES (7,'epijoa-7','LTU10010');
